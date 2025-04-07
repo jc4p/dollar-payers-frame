9ab020e1d384efed6d39e492e91745b150cf4b11
Kasra Rahjerdi <hi@kasra.codes>
2025-04-06 20:10:35 -0700

    We don't need FID at all

--- src/lib/frame.js
+++ src/lib/frame.js
@@ -1,19 +1,5 @@
 import * as frame from '@farcaster/frame-sdk'
 
 export async function initializeFrame() {
-  let user = (await frame.sdk.context).user
-
-  if (user.user) {
-    user = user;
-  }
-
-  if (!user || !user.fid) {
-    // most likely not in a frame
-    return
-  }
-
-  window.userFid = user.fid;
-
-  // Call the ready function to remove your splash screen when in a frame
   await frame.sdk.actions.ready();
 }
